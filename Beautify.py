5b2a94d115a55cb751064a4b8565fe750ee3d38f
Krishan Taylor <krishan@krishan-air.local>
2012-10-22 22:34:30 -0400

    use personal formatting preferences

--- Beautify.py
+++ Beautify.py
@@ -2,29 +2,29 @@ import commands, subprocess
 import sublime, sublime_plugin
 
 class BeautifyCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    self.save()
-    self.prettify(edit)
+    def run(self, edit):
+        self.save()
+        self.prettify(edit)
 
-  def save(self):
-    self.view.run_command("save")
+    def save(self):
+        self.view.run_command("save")
 
-  def prettify(self, edit):
-    scriptPath = sublime.packages_path() + "/Sublime-Beautify/scripts/run.js"
-    setings = ' '.join([
-      "indent_size:\ 2",
-      "indent_char:\ ' '",
-      "max_char:\ 80",
-      "brace_style:\ collapse"
-    ])
-    cmd = ["node",scriptPath,self.view.file_name(),setings]
+    def prettify(self, edit):
+        scriptPath = sublime.packages_path() + "/Sublime-Beautify/scripts/run.js"
+        settings = ' '.join([
+            "indent_size:\ 2",
+            "indent_char:\ ' '",
+            "max_char:\ 80",
+            "brace_style:\ collapse"
+            ])
+        cmd = ["node", scriptPath, self.view.file_name(), settings]
 
-    if sublime.platform()=='windows':
-      p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE)
-      html = p.communicate()[0]
-    else:
-      html = commands.getoutput('"'+'" "'.join(cmd)+'"')
+        if sublime.platform() == "windows":
+            p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+            html = p.communicate()[0]
+        else:
+            html = commands.getoutput('"' + '" "'.join(cmd) + '"')
 
-    if len(html) > 0:
-      self.view.replace(edit, sublime.Region(0, self.view.size()), html.decode('utf-8'))
-      sublime.set_timeout(self.save, 100)
+        if len(html) > 0:
+            self.view.replace(edit, sublime.Region(0, self.view.size()), html.decode("utf-8"))
+            sublime.set_timeout(self.save, 100)
--- scripts/run.js
+++ scripts/run.js
@@ -23,63 +23,58 @@
 
 /*global console, process, require, __dirname */
 
-(function() {
-  "use strict";
+(function () {
+    "use strict";
 
-  var i, len, hash, key, value, raw,
+    var i, len, hash, key, value, raw,
 
-    // cache the console log function and the process arguments
-    log = console.log,
-    argv = process.argv,
+        // cache the console log function and the process arguments
+        log = console.log,
+        argv = process.argv,
 
-    // require path and file system utilities to load the jshint.js file
-    path = require('path'),
-    fs = require('fs'),
+        // require path and file system utilities to load the jshint.js file
+        path = require("path"),
+        fs = require("fs"),
 
-    // the source file to be linted and options
-    source = argv[2] || "",
-    option = {};
+        // the source file to be linted and options
+        source = argv[2] || "",
+        option = {};
 
-  // the style_html and js_beautify functions work when global by dependance
-  global.style_html =
-    require(path.join(__dirname, "beautify-html.js")).style_html;
+    // the style_html and js_beautify functions work when global by dependance
+    global.style_html = require(path.join(__dirname, "beautify-html.js")).style_html;
 
-  global.js_beautify =
-    require(path.join(__dirname, "beautify.js")).js_beautify;
+    global.js_beautify = require(path.join(__dirname, "beautify.js")).js_beautify;
 
-  global.cssbeautify =
-    require(path.join(__dirname, "cssbeautify.js")).cssbeautify;
+    global.cssbeautify = require(path.join(__dirname, "cssbeautify.js")).cssbeautify;
 
-  // continue only if the source file is specified
-  if (source !== "") {
+    // continue only if the source file is specified
+    if (source !== "") {
 
-    // extra arguments with custom options could be passed, so check them now
-    // and add them to the options object
-    for (i = 3, len = argv.length; i < len; i++) {
-      hash = argv[i].split(": ");
-      key = hash[0];
-      value = hash[1];
+        // extra arguments with custom options could be passed, so check them now
+        // and add them to the options object
+        for (i = 3, len = argv.length; i < len; i++) {
+            hash = argv[i].split(": ");
+            key = hash[0];
+            value = hash[1];
 
-      // options are stored in key value pairs, such as option.es5 = true
-      option[key] = value;
+            // options are stored in key value pairs, such as option.es5 = true
+            option[key] = value;
+        }
+
+        // read the source file and, when complete, lint the code
+        fs.readFile(source, "utf8", function (err, data) {
+            if (err) {
+                return;
+            }
+
+            // format the code
+            if (source.match(".html?" + "$")) {
+                log(style_html(data, option));
+            } else if (source.match(".css" + "$") == ".css") {
+                log(cssbeautify(data, option));
+            } else if (source.match(".js" + "$") == ".js") {
+                log(js_beautify(data, option));
+            }
+        });
     }
-
-    // read the source file and, when complete, lint the code
-    fs.readFile(source, "utf8", function(err, data) {
-      if (err) {
-        return;
-      }
-
-      // format the code
-      if (source.match(".html?" + "$")) {
-        log(style_html(data, option));
-      }
-      else if (source.match(".css" + "$") == ".css") {
-        log(cssbeautify(data, option));
-      }
-      else if (source.match(".js" + "$") == ".js") {
-        log(js_beautify(data, option));
-      }
-    });
-  }
 }());
